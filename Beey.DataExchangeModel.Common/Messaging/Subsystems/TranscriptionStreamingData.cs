424cd73f949736b89f2d9464c40438b4dac7e70d
Miloš Kudělka <milos.kudelka@newtontech.cz>
2026-01-17 01:45:56 +0100

    VYV-4396 Add property for heading to TranscriptionStreamingData

--- Beey.DataExchangeModel.Common/Messaging/Subsystems/TranscriptionStreamingData.cs
+++ Beey.DataExchangeModel.Common/Messaging/Subsystems/TranscriptionStreamingData.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Beey.DataExchangeModel.Messaging.Subsystems;
+﻿using Beey.DataExchangeModel.Messaging.Subsystems;
 using Beey.DataExchangeModel.Transcriptions;
 
 namespace Beey.DataExchangeModel.Common.Messaging.Subsystems;
+
 public class TranscriptionStreamingData : SubsystemData<TranscriptionStreamingData>
 {
     public TimeSpan? Transcribed { get; set; }
     public NgPhraseEvent? Word { get; set; }
     public NgSpeakerEvent? SpeakerChangePoint { get; set; }
     public SpeakerIdentificationData? SpeakerData { get; set; }
+    public NgHeadingEvent? Heading { get; set; }
     public TimeSpan? RecognitionLength { get; set; }
 }
