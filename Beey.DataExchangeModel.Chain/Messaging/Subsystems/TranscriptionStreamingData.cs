d37556bf121d4471a76f27d6eb047143808f680c
Ladislav Šeps <ladislav.seps@newtontech.cz>
2026-02-02 11:51:09 +0100

    reduce transcriptionStreaming message size some more by not writing some nulls

--- Beey.DataExchangeModel.Chain/Messaging/Subsystems/TranscriptionStreamingData.cs
+++ Beey.DataExchangeModel.Chain/Messaging/Subsystems/TranscriptionStreamingData.cs
@@ -1,4 +1,5 @@
-﻿using Beey.DataExchangeModel.Messaging.Subsystems;
+﻿using System.Text.Json.Serialization;
+using Beey.DataExchangeModel.Messaging.Subsystems;
 using Beey.DataExchangeModel.Transcriptions;
 
 namespace Beey.DataExchangeModel.Common.Messaging.Subsystems;
@@ -8,9 +9,16 @@ public class TranscriptionStreamingData : SubsystemData<TranscriptionStreamingDa
     public TimeSpan? Transcribed { get; set; }
     public NgPhraseEvent? Word { get; set; }
     public NgSpeakerEvent? SpeakerChangePoint { get; set; }
-    public SpeakerIdentificationData? SpeakerData { get; set; }
-    public NgHeadingEvent? Heading { get; set; }
-
-    public NgPhraseLookAheadEvent[]? Draft { get; set; }
     public TimeSpan? RecognitionLength { get; set; }
+
+    //for backward compatibility maintain writing null on other data
+    //ignore writing null on new V2 data and stuff usefully only internally to reduce message sizes
+    //C# static typed wrappers should be immune to these changes
+    public SpeakerIdentificationData? SpeakerData { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public NgHeadingEvent? Heading { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public NgPhraseLookAheadEvent[]? Draft { get; set; }
+
 }
