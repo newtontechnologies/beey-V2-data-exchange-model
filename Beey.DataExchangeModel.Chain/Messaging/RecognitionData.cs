e8d9c0d96626246d71726774bfe35405c42e5675
Ladislav Šeps <ladislav.seps@newtontech.cz>
2026-02-02 11:41:59 +0100

    renamed lookahead to draft

--- Beey.DataExchangeModel.Chain/Messaging/RecognitionData.cs
+++ Beey.DataExchangeModel.Chain/Messaging/RecognitionData.cs
@@ -1,6 +1,6 @@
-﻿using Beey.DataExchangeModel.Serialization.JsonConverters;
+﻿using System.Text.Json.Serialization;
+using Beey.DataExchangeModel.Serialization.JsonConverters;
 using Beey.DataExchangeModel.Transcriptions;
-using System.Text.Json.Serialization;
 
 namespace Beey.DataExchangeModel.Messaging.Subsystems;
 
@@ -10,11 +10,14 @@ public class RecognitionData : SubsystemData<RecognitionData>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public NgEvent? NgEvent { get; set; }
 
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public NgPhraseLookAheadEvent[]? Draft { get; set; }
+
     [JsonConverter(typeof(JsonNullableConverter<JsonTimeSpanConverter, TimeSpan>))]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public TimeSpan? RecognitionLength { get; set; }
 
-    [JsonIgnore(Condition =JsonIgnoreCondition.WhenWritingNull)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonConverter(typeof(JsonNullableConverter<JsonTimeSpanConverter, TimeSpan>))]
     public TimeSpan? Transcribed { get; set; }
 
